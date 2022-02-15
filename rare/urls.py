bd29c365e9ec3a73094878156449c18212b1ce71
charleslong13 <charleslong13@icloud.com>
2022-02-15 15:23:03 -0600

    csl-premerge

--- rare/urls.py
+++ rare/urls.py
@@ -4,11 +4,12 @@ from django.conf.urls import include
 from rest_framework import routers
 from rareapi.views import register_user, login_user
 from rareapi.views import PostView
+from rareapi.views.tag import TagsView
 
 router = routers.DefaultRouter(trailing_slash=False)
 
 router.register(r'posts', PostView, 'post')
-
+router.register(r'tags', TagsView, 'tag')
 
 urlpatterns = [
     path('register', register_user),
