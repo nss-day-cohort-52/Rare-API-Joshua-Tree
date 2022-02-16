7e7a916c8977e73f5a12fcf99cd2ec9abe107dfe
charleslong13 <charleslong13@icloud.com>
2022-02-16 14:01:18 -0600

    csl-createPost

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -2,10 +2,24 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.serializers import ModelSerializer
-from rareapi.model import Post
-
+from rareapi.model import Post, RareUser, Tag 
 
 class PostView(ViewSet):
+    
+    def create(self, request):
+        user = RareUser.objects.get(pk=request.auth.user_id)
+        tags = []
+
+        for tag_id in request.data['tags']:
+            tag = Tag.objects.get(pk=tag_id)
+            tags.append(tag)
+
+        serializer = CreatePostSerializer(data=request.data)
+        serializer.is_valid()
+        post_obj = serializer.save(user=user)
+        post_obj.tags.set(tags)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
     def list(self, request):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
@@ -21,4 +35,9 @@ class PostSerializer(ModelSerializer):
     class Meta:
         model = Post
         fields = "__all__"
-        depth = 1
+        depth = 2
+
+class CreatePostSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('category', 'title', 'image_url', 'content', 'tags')
